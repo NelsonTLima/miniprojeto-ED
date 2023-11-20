09cf5e2ef23371c30cc8f6e7eebae72cbe5f9423
NelsonTLima <nelsontlima@gmail.com>
2023-11-19 21:26:07 -0300

    Changend Cadastro and Consulta from queue to stack.

--- Painel.py
+++ Painel.py
@@ -1,29 +1,33 @@
-from FilaSequencial import Fila as FilaSequencial
+from PilhaEncadeada import Pilha
 from Senha import Senha
 from Paciente import Paciente
 
 class Painel:
     def __init__(self):
-        self.__cadastro = FilaSequencial(3)
-        self.__consulta = FilaSequencial(3)
-
-        for _ in range(3):
-            self.__cadastro.enfileirar("")
-            self.__consulta.enfileirar("")
+        self.__cadastro = Pilha()
+        self.__consulta = Pilha()
 
     def chamarParaCadastro(self, senha : Senha):
         if not isinstance(senha, Senha):
             return
-        self.__cadastro.desenfileirar()
-        self.__cadastro.enfileirar(senha)
+        self.__cadastro.empilha(senha)
 
     def chamarParaConsulta(self, paciente : Paciente):
         if not isinstance(paciente, Paciente):
             return
-        self.__consulta.desenfileirar()
-        self.__consulta.enfileirar(paciente)
+        self.__consulta.empilha(paciente)
 
     def __str__(self):
+        tamanhoCadastro = len(self.__cadastro)
+        cadastroAtual = "" if tamanhoCadastro < 1 else self.__cadastro.topo()
+        ultimoCadastro = "" if tamanhoCadastro < 2 else self.__cadastro.elemento(tamanhoCadastro - 1)
+        penultimoCadastro = "" if tamanhoCadastro < 3 else self.__cadastro.elemento(tamanhoCadastro - 2)
+
+        tamanhoConsulta = len(self.__consulta)
+        consultaAtual = "" if tamanhoConsulta < 1 else self.__consulta.topo()
+        ultimaConsulta = "" if tamanhoConsulta < 2 else self.__consulta.elemento(tamanhoConsulta - 1)
+        penultimaConsulta = "" if tamanhoConsulta < 3 else self.__consulta.elemento(tamanhoConsulta - 2)
+
         return '''\
        Painel de Atendimento
 Cadastro                Consultório Médico
@@ -31,6 +35,6 @@ Cadastro                Consultório Médico
 > {:>4}                  > {}
 {:>6}                    {}
 {:>6}                    {}'''.format(
-        str(self.__cadastro.elemento(3)), str(self.__consulta.elemento(3)),
-        str(self.__cadastro.elemento(2)), str(self.__consulta.elemento(2)),
-        str(self.__cadastro.elemento(1)), str(self.__consulta.elemento(1)))
+        str(cadastroAtual), str(consultaAtual), # Topo
+        str(ultimoCadastro), str(ultimaConsulta), # Topo -1
+        str(penultimoCadastro), str(penultimaConsulta)) # Topo -2
